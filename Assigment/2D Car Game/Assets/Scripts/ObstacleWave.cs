5d82c9a773e70aa901727ec642d6ddba1f488129
NaiChetcuti <73096475+NaiChetcuti@users.noreply.github.com>
2021-01-07 17:00:40 +0100

    commit

---
+++ Assigment/2D Car Game/Assets/Scripts/ObstacleWave.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleWave : MonoBehaviour
+{
+    [SerializeField] List<Transform> Waypoints;
+    [SerializeField] float moveSpeed = 3f;
+    int waypointIndex = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ObstacleMove()
+    {
+        if (waypointIndex <= Waypoints.Count - 1)
+        {
+            var targetPosition = Waypoints[waypointIndex].transform.position;
+
+            targetPosition.z = 0f;
+
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
